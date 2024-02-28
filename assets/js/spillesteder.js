8074c74a5d10855ac5a535a0236d0b56f08b0d8c
mathiasmundt99 <mathiasbeck99@gmail.com>
2024-02-28 18:11:37 +0100

    Lavet cards + JS. Begyndt på sortering

---
+++ assets/js/spillesteder.js
@@ -0,0 +1,158 @@
+const spillesteder = [
+{
+name: "Kaffefair",
+picture: "spillesteder_kaffefair.png",
+udendørs: false, 
+vin: false,
+madOgDrikke: true,
+bar: false, 
+handicapvenligt: true,
+location: "Strandvejen 19 9000 Aalborg",
+icons: {
+    handicapvenligtIcon: "./assets/img/icons/wheelchair-move-solid.svg", 
+    madOgDrikkeIcon: "./assets/img/icons/wheelchair-move-solid.svg",
+    vinIcon: null, 
+    udendørsIcon: null,
+    drinksIcon: null
+}
+},
+{
+    name: "Bluesscenen",
+    picture: "spillesteder_bluesscenen.png",
+    udendørs: true, 
+    vin: false,
+    madOgDrikke: false,
+    bar: false, 
+    handicapvenligt: true,
+    location: "Tolbod plads, 9000 Aalborg",
+    icons: {
+        handicapvenligtIcon: "wheelchair-move.solid.svg", 
+        madOgDrikkeIcon: null,
+        vinIcon: null, 
+        udendørsIcon: "wheelchair-move.solid.svg",
+        drinksIcon: null
+    }
+},
+{
+    name: "Jazzscenen",
+    picture: "spillesteder_jazzscenen.png",
+    udendørs: true, 
+    vin: false,
+    madOgDrikke: false,
+    bar: false, 
+    handicapvenligt: true,
+    location: "Gammeltorv, 9000 Aalborg",
+    icons: {
+        handicapvenligtIcon: "wheelchair-move.solid.svg", 
+        madOgDrikkeIcon: null,
+        vinIcon: null, 
+        udendørsIcon: "wheelchair-move.solid.svg",
+        drinksIcon: null
+    }
+},
+{
+    name: "D'Wine Bar",
+    picture: "spillesteder_dwine.png",
+    udendørs: false, 
+    vin: true,
+    madOgDrikke: false,
+    bar: false, 
+    handicapvenligt: true,
+    location: "Algade 54, 9000 Aalborg",
+    icons: {
+        handicapvenligtIcon: "wheelchair-move.solid.svg", 
+        madOgDrikkeIcon: null,
+        vinIcon: "./assets/img/icons/wine-glass-solid.svg", 
+        udendørsIcon: null,
+        drinksIcon: null
+    }
+},
+{
+    name: "Salling ROOGTOP",
+    picture: "spillesteder_salling.png",
+    udendørs: true, 
+    vin: false,
+    madOgDrikke: true,
+    bar: true, 
+    handicapvenligt: true,
+    location: "Nytorv 8, 9000 Aalborg",
+    icons: {
+        handicapvenligtIcon: "wheelchair-move.solid.svg", 
+        madOgDrikkeIcon: "wheelchair-move.solid.svg",
+        vinIcon: null, 
+        udendørsIcon: null,
+        drinksIcon: null
+    }
+},
+
+]
+
+const cardDivEl = document.querySelector(".cardDiv"); 
+
+/* Her lavers en funktion til at kunne finde de forskellige ikoner til spillestederne*/
+function generateIconsHTML(icons) {
+    let iconsHTML = '';
+  
+    /* Vi bruge denne metode til at loope igennem alle icons' keys og values i vores Array
+    object.entries bruges til ay konvertere icons indtil et array af keys og values
+    */
+    Object.entries(icons).forEach(([key, value]) => {
+        /* Her bruges en if statement til at finde ud af om value IKKE er null*/
+      if (value !== null) {
+        /* Her bliver de forskellige ikoners value sat som src, hvilket er stien som er angivet i arrayet, og alt værdien = vores key. Fx: handicapvenligtIcon */
+        iconsHTML += `<img src="${value}" alt="${key}">`;
+      }
+    });
+    
+    return iconsHTML;
+  }
+
+  /* 
+  Vi bruger et forEach loop til at generere indhold på vores HTML side 
+  Her bruges funktionen som blev lavet ovenover til at indsætte diverse ikoner hvis de ikke = null i arrayet
+  */
+spillesteder.forEach(function (sted) {
+    cardDivEl.innerHTML += `
+      <div class="cardDiv">
+      <div class="spillested">
+        <h3>${sted.name}</h3>
+        <div>
+          <img src="./assets/img/pictures/spillesteder/${sted.picture}" alt="">
+        </div>
+        <p>${sted.location}</p>
+        <div class="icons">
+          ${generateIconsHTML(sted.icons)}
+        </div>
+        <a href="#"><button>Se hele programmet her</button></a>
+        </div>
+      </div>
+    `;
+  });
+
+
+  function sorterSpillesteder(spillesteder) {
+    cardDivEl.innerHTML = '';
+    spillesteder.forEach(sted => {
+        cardDivEl.innerHTML += generateSpillested(sted);
+    })
+  }
+  
+  // Map button classes to corresponding property names
+  function mapClassToFilterKey(filterClass) {
+    switch (filterClass) {
+      case 'madOgDrikkeBtn':
+        return 'madOgDrikke';
+      case 'barBtn':
+        return 'bar'; 
+      case 'vinbarBtn':
+        return 'vin';
+      case 'udendøresBtn':
+        return 'udendørs';
+      case 'handicapvenligBtn':
+        return 'handicapvenligt';
+      default:
+        return null;
+    }
+  }
+  
+
