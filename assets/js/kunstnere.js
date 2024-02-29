92cbe39e20c491a1c21d68b3b3029b9a3d905c99
Amalieandreasen <148330357+Amalieandreasen@users.noreply.github.com>
2024-02-29 13:33:15 +0100

    javascript funker

--- assets/js/kunstnere.js
+++ assets/js/kunstnere.js
@@ -1,3 +1,5 @@
+//Javascript til Adam Karch
+
 let popUpBox = document.getElementById("popUp")
 
 function displayPopup(){
@@ -8,5 +10,40 @@ function closePopup(){
     popUpBox.classList.remove("display");
 };
 
+//Javascript til Advokatens New Orleans Jazzband
+
+let popUpBox1 = document.getElementById("popUp1")
+
+function displayPopup1(){
+popUpBox1.classList.add("display");
+};
+
+function closePopup1(){
+    popUpBox1.classList.remove("display");
+};
+
+//Javascript til Alain Apaloo Trio
+
+let popUpBox2 = document.getElementById("popUp2")
+
+function displayPopup2(){
+popUpBox2.classList.add("display");
+};
+
+function closePopup2(){
+    popUpBox2.classList.remove("display");
+};
+
+//Javascript til All Time Jazz Band
+
+let popUpBox3 = document.getElementById("popUp3")
+
+function displayPopup3(){
+popUpBox3.classList.add("display");
+};
+
+function closePopup3(){
+    popUpBox3.classList.remove("display");
+};
 
 
