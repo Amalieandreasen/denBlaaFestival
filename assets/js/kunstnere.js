b1b8086b7292e6c2bb9f9cbaba5177262190dca8
Amalieandreasen <148330357+Amalieandreasen@users.noreply.github.com>
2024-02-29 11:09:04 +0100

    updated

---
+++ assets/js/kunstnere.js
@@ -0,0 +1,9 @@
+let popUpBox = document.getElementById("popUp")
+
+function displayPopup(){
+popUpBox.classList.add("display");
+};
+
+function closePopup(){
+    popUpBox.classList.remove("display");
+};
